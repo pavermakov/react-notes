59a97c0d92cc58e51fc7956d46b36bc6963be738
Pavel Ermakov <pavermakov@PavelErakovsMBP.Dlink>
2018-01-01 23:55:56 +0300

    WEB-1. Added board fallback

---
+++ src/components/ghost-text/GhostText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './GhostText.less';
+
+const GhostText = () => (
+  <span className="ghost-text">double tap to add a note</span>
+);
+
+export default GhostText;
